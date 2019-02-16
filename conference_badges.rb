a553b4f401fa1906a4af0cc59a0b6d6c606847b3
Dan Suarez <suarez.daniel101@gmail.com>
2019-02-16 03:00:31 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -21,7 +21,7 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-  batch_badge_creator(attendees).each do |attendee|
-    puts attendee
+  batch_badge_creator(attendees).each do |attendees|
+    puts attendees
   end
 end
